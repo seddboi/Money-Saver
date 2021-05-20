b0177f4621977ceaa27ae511a912da026a09b37d
Gian Zamora <gz2016@att.net>
2021-05-19 17:44:09 -0700

    updated server,js with full sequelize server code

---
+++ server.js
@@ -0,0 +1,36 @@
+const app = express();
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
+const path = require('path');
+
+const help = require('./utils/helpers');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({helpers});
+
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3306;
+
+const SequelizeStore = require('connect-sesssion-sequalize')(sess.Store);
+
+const sess = {
+    secret: 'the lords cheeps',
+    cookie: {},
+    resave: false,
+    saveUninitialized: false,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequalize.sync({ force: false }).then( () => {
+    app.listen(PORT, () => console.log('Now listening'))
+});
