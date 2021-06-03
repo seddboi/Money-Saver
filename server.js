9574075cd177ffc3695ed13cc76f2ede0d0a67a7
Gian Zamora <gz2016@att.net>
2021-06-02 21:55:56 -0700

    uncommented actual server code

--- server.js
+++ server.js
@@ -1,57 +1,58 @@
 // this is the main code for the server connection
 
-// const express = require('express');
-// const sequelize = require('./config/connection');
-// const routes = require('./controllers');
-// const path = require('path');
-
-// // const help = require('./utils/helpers');
-
-// const exphbs = require('express-handlebars');
-// // const hbs = exphbs.create({helpers});
-
-// const session = require('express-session');
-
-// const app = express();
-// const port = process.env.port || 3306;
-
-// // const SequelizeStore = require('connect-sesssion-sequelize')(sess.Store);
-
-// // // const sess = {
-// //     secret: 'the lords cheeps',
-// //     cookie: {},
-// //     resave: false,
-// //     saveUninitialized: false,
-// //     store: new SequelizeStore({
-// //         db: sequelize,
-// //     }),
-// // };
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true}));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use(routes);
-
-// sequelize.sync({ force: false }).then( () => {
-//     app.listen(port, () => console.log('Now listening'))
-// });
-
-// ----------------------------------------------------------------------------------
-// This is used to test the teller-connect button page
-
 const express = require('express');
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
 const path = require('path');
 
-// Sets up the Express App
+const help = require('./utils/helpers');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({helpers});
+
+const session = require('express-session');
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.port || 3306;
 
+const SequelizeStore = require('connect-sesssion-sequelize')(sess.Store);
+
+const sess = {
+    secret: 'the lords cheeps',
+    cookie: {},
+    resave: false,
+    saveUninitialized: false,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
-// Sets up the routes
-app.use(require('./controllers/index'));
 
-// Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log('Server listening on: http://localhost:' + PORT);
+app.use(routes);
+
+sequelize.sync({ force: false }).then( () => {
+    app.listen(port, () => console.log('Now listening'))
 });
+
+// ----------------------------------------------------------------------------------
+// DISCONTINUED USE OF THIS
+// This is used to test the teller-connect button page
+
+// const express = require('express');
+// const path = require('path');
+
+// // Sets up the Express App
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// app.use(express.static(path.join(__dirname, 'public')));
+// // Sets up the routes
+// app.use(require('./controllers/index'));
+
+// // Starts the server to begin listening
+// app.listen(PORT, () => {
+//   console.log('Server listening on: http://localhost:' + PORT);
+// });
