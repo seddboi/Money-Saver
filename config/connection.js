d691350587c4fe26087c68a3ae1422eeb28e75be
Gian Zamora <gz2016@att.net>
2021-07-05 20:02:53 -0700

    updated to match code for easier push to main

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 let sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.DB_PASSWORD,
     {
         host: 'localhost',
         dialect: 'mysql',
